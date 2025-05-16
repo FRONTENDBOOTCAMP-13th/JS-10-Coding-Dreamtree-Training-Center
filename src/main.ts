81ee447c9adcf345fc967828e852867049f5ee15
jjmullan <jjmullan24@gmail.com>
2025-05-16 10:55:31 +0900

    🔧 fix: main.ts 에서 사용되지 않는 파일 링크 해제 - X modalRendering.ts Ref #40

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import './style.css';
 import './components/Sidebar/Sidebar.ts';
 import './script/resourceRendering.ts';
-import './script/modalRendering.ts';
+// import './script/modalRendering.ts';
