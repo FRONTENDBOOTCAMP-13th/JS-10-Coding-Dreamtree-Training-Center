8156ef455f2f4d9697f3f7456980680db26f8bdf
toosign <kevinsoo1014@gmail.com>
2025-05-19 15:25:36 +0900

    💄 style: 리소스 카드 스타일 변경 줄넘김, 레이아웃 배치 일관되도록 수정 Ref #32

--- src/script/resourceRendering.ts
+++ src/script/resourceRendering.ts
@@ -39,59 +39,59 @@ export function createResourceArticle(resource: Resource): string {
 
   return `
     <article
-      class="text-quokka-black rounded-[.625rem] bg-white px-7 py-5 text-xs font-medium shadow-[0_4px_4px_0_rgba(139,109,92,0.10)]" data-index="${resource.id}"
-    >
-      <h3 class="text-2xl font-semibold" data-roll="title">${resource.title}</h3>
-      <p class="text-quokka-gray mt-2 text-sm" data-roll="description">${resource.description}</p>
-      <div class="mt-4 font-semibold" data-roll="tags">${tagsHtml}</div>
-      <div class="mt-5 flex flex-row justify-between">
-        <div class="flex flex-row gap-3">
-          <span class="flex flex-row items-center gap-1"  data-roll="category">
-          <svg
-                  width="15"
-                  height="15"
-                  viewBox="0 0 15 15"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M5.64317 1H2.59061C1.71214 1 1 1.71214 1 2.59061V5.64317C1 6.06502 1.16758 6.4696 1.46588 6.7679L8.23911 13.5411C8.73302 14.035 9.49729 14.1573 10.0817 13.7747C11.5525 12.8118 12.8118 11.5525 13.7747 10.0817C14.1573 9.49729 14.035 8.73302 13.5411 8.23911L6.7679 1.46588C6.4696 1.16758 6.06502 1 5.64317 1Z"
-                    stroke="#121212"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M3.12081 3.12081H3.12611V3.12611H3.12081V3.12081Z"
-                    stroke="#121212"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-                ${resource.category}</span>
-          <span class="flex flex-row items-center gap-1" data-roll="difficulty">
-          <svg
-                  width="15"
-                  height="15"
-                  viewBox="0 0 15 15"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M7.21191 0C11.1944 0.000203187 14.4226 3.22939 14.4229 7.21191C14.4226 11.1944 11.1944 14.4226 7.21191 14.4229C3.22941 14.4226 0.000203183 11.1944 0 7.21191C0.000203002 3.22941 3.22941 0.000230065 7.21191 0ZM7.21191 1.5C4.05784 1.50023 1.5002 4.05783 1.5 7.21191C1.5002 10.366 4.05784 12.9226 7.21191 12.9229C10.366 12.9226 12.9226 10.366 12.9229 7.21191C12.9226 4.05782 10.366 1.5002 7.21191 1.5ZM7.21191 3.62207C9.19447 3.62207 10.8018 5.22935 10.8018 7.21191C10.8018 9.19447 9.19447 10.8018 7.21191 10.8018C5.22938 10.8017 3.62207 9.19446 3.62207 7.21191C3.62207 5.22937 5.22938 3.6221 7.21191 3.62207Z"
-                    fill="${difficultyStroke}"
-                  />
-                </svg>
-                ${resource.difficulty}</span>
+            class="flex h-auto flex-col gap-3 rounded-2xl bg-white px-7 py-5 text-xs font-medium shadow-lg transition hover:shadow-xl"
+      >
+      <h3 class="text-quokka-black line-clamp-2 text-2xl font-semibold">${resource.title}</h3>
+      <p class="text-quokka-black mb-1 line-clamp-2 overflow-hidden text-sm">${resource.description}</p>
+      <div class="mb-2 flex gap-2" data-roll="tags">${tagsHtml}</div>
+      <div class="mb-2 flex flex-wrap items-center justify-between gap-3">
+        <div class="flex flex-wrap items-center gap-3">
+          <span class="flex items-center gap-1" data-roll="category">
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M5.64317 1H2.59061C1.71214 1 1 1.71214 1 2.59061V5.64317C1 6.06502 1.16758 6.4696 1.46588 6.7679L8.23911 13.5411C8.73302 14.035 9.49729 14.1573 10.0817 13.7747C11.5525 12.8118 12.8118 11.5525 13.7747 10.0817C14.1573 9.49729 14.035 8.73302 13.5411 8.23911L6.7679 1.46588C6.4696 1.16758 6.06502 1 5.64317 1Z"
+                stroke="#121212"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M3.12081 3.12081H3.12611V3.12611H3.12081V3.12081Z"
+                stroke="#121212"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            ${resource.category}</span>
+          <span class="flex items-center gap-1" data-roll="difficulty">
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7.21191 0C11.1944 0.000203187 14.4226 3.22939 14.4229 7.21191C14.4226 11.1944 11.1944 14.4226 7.21191 14.4229C3.22941 14.4226 0.000203183 11.1944 0 7.21191C0.000203002 3.22941 3.22941 0.000230065 7.21191 0ZM7.21191 1.5C4.05784 1.50023 1.5002 4.05783 1.5 7.21191C1.5002 10.366 4.05784 12.9226 7.21191 12.9229C10.366 12.9226 12.9226 10.366 12.9229 7.21191C12.9226 4.05782 10.366 1.5002 7.21191 1.5ZM7.21191 3.62207C9.19447 3.62207 10.8018 5.22935 10.8018 7.21191C10.8018 9.19447 9.19447 10.8018 7.21191 10.8018C5.22938 10.8017 3.62207 9.19446 3.62207 7.21191C3.62207 5.22937 5.22938 3.6221 7.21191 3.62207Z"
+                fill="${difficultyStroke}"
+              />
+            </svg>
+            ${resource.difficulty}</span>
         </div>
-        <span data-roll="dateAdded">${resource.dateAdded}</span>
+        <span class="flex-shrink-0" data-roll="dateAdded">${resource.dateAdded}</span>
       </div>
-      <div class="mt-6 flex flex-row items-center justify-between">
+      <div class="mt-auto flex flex-row items-center justify-between">
         <button
           type="button"
           name="detail"
-          class="bg-quokka-brown gap-4 self-center rounded-4xl px-5 py-1.5 text-sm font-semibold text-white cursor-pointer"
+          class="bg-quokka-brown cursor-pointer gap-4 self-center rounded-4xl px-5 py-1.5 text-sm font-semibold text-white"
         >
           상세보기
         </button>
@@ -128,7 +128,6 @@ export async function renderResources(list: Resource[]): Promise<void> {
   const section = document.querySelector('main > section');
 
   if (!section) return;
-  section.innerHTML = resources.map(createResourceArticle).join('');
 
   // 필터링된 데이터가 존재하지 않는 경우, 대체 텍스트를 생성
   if (list.length === 0) {
@@ -142,7 +141,13 @@ export async function renderResources(list: Resource[]): Promise<void> {
     return;
   }
 
-  section.innerHTML = list.map(createResourceArticle).join('');
+  // 리소스 데이터에 인덱스 추가하여 렌더링
+  section.innerHTML = list
+    .map((resource, index) => {
+      const articleHtml = createResourceArticle(resource);
+      return articleHtml.replace('<article', `<article data-index="${index + 1}"`);
+    })
+    .join('');
 
   // ↓ 모달 관련 기능
   const detailBtn = section.querySelectorAll<HTMLButtonElement>('button[name="detail"]');
